49ea9e066b8b7f19949662b7b351b5817fdd8f37
el-bana <ibrahimaelbana@gmail.com>
2022-03-12 20:17:34 +0200

    some fixes

--- app.js
+++ app.js
@@ -7,20 +7,6 @@ const TOKEN = process.env.MASTER_ACCESS_TOKEN;
 const FETCH_INTERVAL = process.env.FETCH_INTERVAL * 1000;
 const BLOCKED_KEYWORDS = process.env.BLOCKED_KEYWORDS;
 
-const jobsLogTemplate = (job) => {
-  const jobURL = "https://www.upwork.com/jobs/" + job.ciphertext;
-  return `
-    ======================
-    ${job.title}
-    Description: ${job.description.slice(0, 50)}...
-    ${jobURL}
-    ${dateFns.formatDistanceToNow(dateFns.parseISO(job.publishedOn), {
-      addSuffix: true,
-    })}
-    Budget: ${job.hourlyBudgetText || job.amount.amount + "$"}
-    ======================`;
-};
-
 let latestJobTime = new Date(0);
 
 async function fetchJobs() {
@@ -48,22 +34,87 @@ async function fetchJobs() {
   }
 }
 
+/**
+ * Sets the time of the latest job
+ *
+ *
+ * @param {String} time string representation of time
+ */
+
 function setLatestJobTime(time) {
   latestJobTime = new Date(time);
 }
 
+/**
+ * Identifies the new jobs coming from fetch result
+ *
+ *
+ * @param {Array} jobs Array of jobs
+ * @returns {Array} array of new jobs
+ */
+
 function identifyNewJobs(jobs) {
-  const filteredJobs = jobs.filter((job) => {
+  const newJobs = jobs.filter((job) => {
     if (dateFns.isAfter(dateFns.parseISO(job.publishedOn), latestJobTime))
       return true;
   });
 
-  if (filteredJobs.length > 0) {
-    setLatestJobTime(filteredJobs[0].publishedOn);
+  if (newJobs.length > 0) {
+    setLatestJobTime(newJobs[0].publishedOn);
   }
 
-  return filteredJobs;
+  return newJobs;
 }
+
+/**
+ * Remove jobs that contains one of the blocked keywords in title
+ * or description.
+ *
+ * @param {Array} jobs Array of jobs
+ * @returns array of filtered results
+ */
+
+function filterBlocked(jobs) {
+  let arrayOfKeywords = BLOCKED_KEYWORDS.split(",");
+
+  const checker = (job) => {
+    return !arrayOfKeywords.some(
+      (keyword) =>
+        job.title.toLowerCase().includes(keyword) ||
+        job.description.toLowerCase().includes(keyword)
+    );
+  };
+
+  return jobs.filter(checker);
+}
+
+/**
+ * converts a job to a redable representation for logging
+ *
+ *
+ * @param {Array} jobs Array of jobs
+ * @returns {String} a string representation for the job
+ */
+
+const jobsLogTemplate = (job) => {
+  const jobURL = "https://www.upwork.com/jobs/" + job.ciphertext;
+  return `
+    ======================
+    ${job.title}
+    Description: ${job.description.slice(0, 50)}...
+    ${jobURL}
+    ${dateFns.formatDistanceToNow(dateFns.parseISO(job.publishedOn), {
+      addSuffix: true,
+    })}
+    Budget: ${job.hourlyBudgetText || job.amount.amount + "$"}
+    ======================`;
+};
+
+/**
+ * Logs the jobs in the console so user can see new jobs
+ *
+ * @param {Array} jobs Array of jobs
+ */
 
 function logJobs(jobs) {
   if (jobs.length) {
@@ -81,29 +132,14 @@ function logJobs(jobs) {
   }
 }
 
+/**
+ * alerts the number of new jobs found
+ *
+ * @param {number} length
+ */
+
 function alertNewJobs(length) {
   alert("New " + length + " jobs found");
-}
-
-/**
- * Remove jobs that contains one of the blocked keywords in title
- * or description.
- *
- * @param {Array} jobs Array of jobs
- * @returns array of filtered results
- */
-function filterBlocked(jobs) {
-  let arrayOfKeywords = BLOCKED_KEYWORDS.split(",");
-
-  const checker = (job) => {
-    return !arrayOfKeywords.some(
-      (keyword) =>
-        job.title.toLowerCase().includes(keyword) ||
-        job.description.toLowerCase().includes(keyword)
-    );
-  };
-
-  return jobs.filter(checker);
 }
 
 async function main() {
